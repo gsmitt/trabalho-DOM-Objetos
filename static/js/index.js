e2c1eb5b39998f02f2a4388a745daa29c72c8d14
Brunomatheusferrari <brunomatheussferrari@gmail.com>
2021-07-08 15:14:14 -0300

    alterações css

---
+++ static/js/index.js
@@ -0,0 +1,22 @@
+function acao() {
+    let modal = document.querySelector(".modalBg")
+
+    modal.style.display = 'flex';
+}
+
+function fechar() {
+    let modal = document.querySelector(".modalBg")
+
+    modal.style.display = 'none';
+}
+
+// let inputTurmaModal = document.querySelector(".modalInputTurma") 
+// let inputEscolaModal = document.querySelector(".modalInputEscola") 
+// let botaoCadastrarTurma = document.querySelector(".botaoCadastrarTurma")
+// let formModal = document.querySelector(".formModal")
+// let modal = document.querySelector(".modalBg")
+
+// botaoCadastrarTurma.addEventListener('click', (event) => {
+//     let div = document.createElement("div")
+//     let textoDiv = document.createTextNode(inputTurmaModal.value)
+// })
