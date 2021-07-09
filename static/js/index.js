7a3982881b0845808a0d9998f9eecdef5f7255db
Brunomatheusferrari <brunomatheussferrari@gmail.com>
2021-07-09 01:57:03 -0300

    css2

--- static/js/index.js
+++ static/js/index.js
@@ -10,13 +10,3 @@ function fechar() {
     modal.style.display = 'none';
 }
 
-// let inputTurmaModal = document.querySelector(".modalInputTurma") 
-// let inputEscolaModal = document.querySelector(".modalInputEscola") 
-// let botaoCadastrarTurma = document.querySelector(".botaoCadastrarTurma")
-// let formModal = document.querySelector(".formModal")
-// let modal = document.querySelector(".modalBg")
-
-// botaoCadastrarTurma.addEventListener('click', (event) => {
-//     let div = document.createElement("div")
-//     let textoDiv = document.createTextNode(inputTurmaModal.value)
-// })
