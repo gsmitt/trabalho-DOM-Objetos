db2e5a3387998b46515c28a560e9d20e9753b5fd
Gustavo <gundelhausmann@gmail.com>
2021-07-11 04:25:21 -0300

    iniciando o DOM do site

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,61 @@
+// Imports/Exports
+import {turmas, salvarTurma} from "./funcoes.js";
+import {Turma} from "./turma.js";
+export {acao, fechar, submit}
+
+// Prepara a página
+window.onload = () =>{ 
+    let cria = document.querySelector("#criarTurmaButton"),
+    fecha = document.querySelector(".closeButtonModal"),
+    sub = document.querySelector(".botaoCadastrarTurma")
+
+    cria.addEventListener("click", () => {
+        acao()
+    })
+
+    fecha.addEventListener("click", () => {
+        fechar()
+    })
+
+    sub.addEventListener("click", () => {
+        submit()
+    })
+
+    prepara()
+}
+
+function prepara(){
+    if (turmas[0]){
+        let naoTem = document.querySelector(".nenhumaNota"),
+        sect = document.querySelector(".section2")
+        naoTem.style.display = "none"
+        imprimeTurmas(sect)
+    }
+}
+
+
+function imprimeTurmas(sect){
+    sect.innerHTML = ""
+    for(let i of turmas){
+        let div = document.createElement("div"),
+        nome = document.createElement("h2"),
+        codigo = document.createElement("p")
+        
+        div.addEventListener("click",() => {
+            sessionStorage.setItem("turma", i.codigoTurma)
+            window.location.href = "../../turma.html"
+        })
+
+        nome.textContent = i.nomeTurma
+        codigo.textContent = i.codigoTurma
+        div.appendChild(nome)
+        div.appendChild(codigo)
+        sect.appendChild(div)
+    }
+}
+
+
+// Modal
 function acao() {
     let modal = document.querySelector(".modalBg")
 
@@ -10,3 +68,36 @@ function fechar() {
     modal.style.display = 'none';
 }
 
+function submit()  {
+    let nome = document.querySelector(".modalInputTurma"),
+    codigo = document.querySelector(".modalInputEscola"),
+    pnome = document.querySelector(".pNome"),
+    pcodigo = document.querySelector(".pCodigo")
+
+    if (!nome.value || !codigo.value){
+        if (!nome.value){
+            pnome.textContent = "Campo não preenchido";
+        }
+        else{
+            pnome.textContent = " ";
+        }
+        if (!codigo.value){
+            pcodigo.textContent = "Campo não preenchido";
+        }
+        else{
+            pcodigo.textContent = " ";
+        }
+    }
+    else{
+        pnome.textContent = " ";
+        let novaTurma = new Turma(codigo.value,nome.value)
+        let x = salvarTurma(novaTurma)
+        prepara()
+        if (!x){
+            pcodigo.textContent = "Este código já existe";
+        }
+        else{
+            fechar()
+        }
+    }
+}
---
+++ static/js/paginaTurma.js
@@ -0,0 +1,74 @@
+//Export/Import
+import {turmas, pegaTurma, salvarTurma} from "./funcoes.js";
+export {acao,fechar,acao2,acao3}
+
+//Prepara a página
+window.onload = () =>{ 
+    let turma = sessionStorage.getItem("turma"),
+    editaT = document.querySelector(".editarNomeTurma"),
+    editaC = document.querySelector(".editarNomeEscola"),
+    cadastra = document.querySelector(".cadastrarAluno"),
+    fecha = document.querySelectorAll(".closeButtonModal"),
+    titulo = document.querySelector(".titleTurma"),
+    subt = document.querySelector(".subtitleEscola")
+
+    editaT.addEventListener("click", () => {
+        acao()
+    })
+
+    editaC.addEventListener("click", () => {
+        acao2()
+    })
+
+    cadastra.addEventListener("click", () => {
+        acao3()
+    })
+
+    for (let i of fecha){
+        i.addEventListener("click", () => {
+            fechar()
+        })
+    }
+
+    let atual = pegaTurma(turma)
+    titulo.innerHTML = atual.nomeTurma
+    subt.innerHTML = atual.codigoTurma
+
+    //Submit Modal's
+    let subm1 = document.querySelector(".modalBg .botaoCadastrarTurma"),
+    subm2 = document.querySelector(".modalBg2 .botaoCadastrarTurma"),
+    subm3 = document.querySelector(".botaoCadastrarAlunoModal")
+
+    //subm1.addEventListener("click",() => //Criar uma Function Muda Turma{})
+
+}
+
+//Modal's de alterar nome
+
+function acao() {
+    let modal = document.querySelector(".modalBg")
+
+    modal.style.display = 'flex';
+}
+
+function fechar() {
+    let modal = [document.querySelector(".modalBg"), document.querySelector(".modalBg2"), document.querySelector(".modalBg3")]
+    for (let i of modal){
+        i.style.display = 'none';
+        }
+    
+}
+
+function acao2() {
+    let modal = document.querySelector(".modalBg2")
+
+    modal.style.display = 'flex';
+}
+
+
+function acao3() {
+    let modal = document.querySelector(".modalBg3")
+
+    modal.style.display = 'flex';
+}
+
