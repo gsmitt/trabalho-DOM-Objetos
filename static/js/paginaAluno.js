7e4c8504ddf6efc663bfaa6dcda71262086229a1
Bruno Matheus Ferrari <brunomatheussferrari@gmail.com>
2021-07-13 16:50:10 -0300

    Ta prontooo

--- static/js/paginaAluno.js
+++ static/js/paginaAluno.js
@@ -13,11 +13,13 @@ window.onload = () => {
     calcular = document.querySelector(".botaoMedia"),
     fecha = document.querySelectorAll(".closeButtonModal"),
     titulo = document.querySelector(".headerTitle"),
-    subt = document.querySelector(".headerSubtitle")
+    subt = document.querySelector(".headerSubtitle"),
+    lixeirinha = document.querySelector("#lixeirinha"),
+    setinha = document.querySelector("#setinhaBack")
 
     aluno = new Aluno(aluno.matricula, aluno.nome, aluno.telefone, aluno.email, aluno.notas)
-    
-    let turmaAtual = pegaTurma(turma) 
+    aluno.calculaMedia()
+    let turmaAtual = pegaTurma(turma)
     
 
     //header
@@ -31,14 +33,21 @@ window.onload = () => {
     cadastrar.addEventListener("click", () => {
         acao()
     })
-    calcular.addEventListener("click", () => {
 
-    })
     for (let i of fecha){
-        i.addEventListener("click", () => {
+        i.addEventListener("click", (event) => {
+            event.preventDefault()
             fechar()
         })
     }
+    lixeirinha.addEventListener("click", () => {
+        deleteAluno(turma, matricula)
+    })
+
+    setinha.addEventListener("click", () => {
+        window.location.href= "./turma.html"
+    })
+
     //informações
     let pnome = document.querySelector(".paragrafoName"),
     pmatri = document.querySelector(".paragrafoRegistration"),
@@ -51,7 +60,8 @@ window.onload = () => {
     pemail.innerHTML += aluno.email
 
     // Submits
-    let subm1 = document.querySelector(".botaoAlterarInformacoesModal")
+    let subm1 = document.querySelector(".botaoAlterarInformacoesModal"),
+    subm2 = document.querySelector(".botaoCadastrarNotasModal")
 
     subm1.addEventListener("click", (event) => {
         event.preventDefault()
@@ -60,10 +70,23 @@ window.onload = () => {
         tele = document.querySelector(".telefoneAluno").value,
         ema = document.querySelector(".emailAluno").value
         if (turmaAtual.cadastrarAluno(new Aluno(matr, nom, tele, ema))){
-            mudaAluno(turma, matricula, nom, matr, tele, ema)
+            mudaAluno(turma, matricula, nom, matr, tele, ema, "")
             location.reload()
         }
     })
+    subm2.addEventListener("click", (event) => {
+        event.preventDefault()
+        let n1 = document.querySelector(".nota1").value,
+        n2 = document.querySelector(".nota2").value,
+        n3 = document.querySelector(".nota3").value,
+        notas = [n1,n2,n3]
+
+        mudaAluno(turma, matricula, "", "", "", "", notas)
+        location.reload()
+    })
+
+    //Notas
+    verificaNotas(aluno)
 }
 // Modal
 function acao() {
@@ -86,3 +109,57 @@ function acao2() {
     modal.style.display = 'flex';
 }
 
+//Preparando
+function verificaNotas(aluno){
+    let n1,n2,n3
+    if(aluno.notas[0]){
+        n1 = true
+    }
+    if(aluno.notas[1]){
+        n2 = true
+    }
+    if(aluno.notas[2]){
+        n3 = true
+    }
+    if (n1 == true || n2 == true || n3 == true){
+        let nada = document.querySelector(".semNotaCadastrada")
+        nada.style.display = "none"
+        imprimeNotas(aluno, n1, n2, n3)
+    }
+}
+
+function imprimeNotas(aluno, n1, n2, n3){
+    let container = document.querySelector(".notasAluno"),
+    div = document.createElement("div"),
+    nt1 = document.createElement("p"),
+    nt2 = document.createElement("p"),
+    nt3 = document.createElement("p"),
+    medi = document.createElement("p")
+
+    div.classList.add("listaGrades")
+
+
+    if(n1){nt1.textContent = "Nota 1: " + aluno.notas[0]}
+    if(n2){nt2.textContent = "Nota 2: " + aluno.notas[1]}
+    if(n3){nt3.textContent = "Nota 3: " + aluno.notas[2]}
+    if(n1 && n2 && n3){medi.textContent = "Media: " + aluno.media.toFixed(2)}
+    div.appendChild(nt1)
+    div.appendChild(nt2)
+    div.appendChild(nt3)
+    div.appendChild(medi)
+    container.appendChild(div)
+}
+
+function deleteAluno(codigoTurma,matricula){
+    let turmas = JSON.parse(localStorage.getItem("turmas"))
+    for (let i of turmas){
+        if (i.codigoTurma == codigoTurma){
+            for (let j of i.alunos)
+                if (j.matricula == matricula){
+                    i.alunos.splice(j,1)
+                }
+            localStorage.setItem("turmas",JSON.stringify(turmas))
+            window.location.href = "./turma.html"
+        }
+    }
+}
